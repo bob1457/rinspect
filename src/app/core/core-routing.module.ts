3d904848d5ebb48d322d04e6cccf1b56c61d1dae
bob <bob.yuan@yahoo.com>
2020-08-17 15:18:00 -0700

    Add more pages and styles

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ReportComponent } from '../report/report.component';
+import { ReportComponent } from '../core/report/report.component';
+import { AddReportComponent } from './add-report/add-report.component';
+import { ReportDetailsComponent } from './report-details/report-details.component';
 
 
 const routes: Routes = [
@@ -9,6 +11,8 @@ const routes: Routes = [
     component: HomeComponent,
     children: [
       { path: '', redirectTo: 'report', pathMatch: 'full'},
+      { path: 'add', component: AddReportComponent},
+      { path: 'report-details', component: ReportDetailsComponent},
       { path: 'report', component: ReportComponent}
     ]
   }
