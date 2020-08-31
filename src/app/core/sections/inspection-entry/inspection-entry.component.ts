ab569670b078b680d6016d5b27739bb6ff13bb65
bob <bob.yuan@yahoo.com>
2020-08-31 15:53:07 -0700

    Style report details page

--- src/app/core/sections/inspection-entry/inspection-entry.component.ts
+++ src/app/core/sections/inspection-entry/inspection-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inspection-entry',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InspectionEntryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  submit() {
+    console.log('submitted');
+
+    // this.router.navigate(['/home/addsection']);
+    this.reloadComponent();
+  }
+
+  reloadComponent() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/home/addsection']);
+}
 
 }
--- src/app/core/sections/inspection-kitchen/inspection-kitchen.component.ts
+++ src/app/core/sections/inspection-kitchen/inspection-kitchen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-inspection-kitchen',
@@ -8,14 +10,38 @@ import { Component, OnInit } from '@angular/core';
 export class InspectionKitchenComponent implements OnInit {
 
   addMore = false;
+  addForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.addForm = this.formBuilder.group({
+      name: ['']
+    });
   }
 
-  clicked(event) {
+  checked(event) {
+    // console.log(event);
+    this.addMore = event.checked;
+    console.log(this.addMore); 
+  }
+
+  submit() {
+    if(this.addMore){
+      this.addForm.reset();
+      // this.router.navigate(['/home/addsection']);      
+      this.addMore = false;
+    } else {
+      this.reloadComponent();
+    }
     
+  }
+
+  reloadComponent() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/home/addsection']);
   }
 
 }
