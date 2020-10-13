5d7898e545f1c9abb192a2ba3c9379c9faba91c3
bob <bob.yuan@yahoo.com>
2020-10-13 15:43:00 -0700

    Enhance login UI

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -12,6 +12,9 @@ export class SigninComponent implements OnInit {
 
   signinForm: FormGroup;
 
+  loading = false;
+  errMsg = '';
+
   constructor(private formBuilder: FormBuilder,
               private authService: AuthService,
               private router: Router) { }
@@ -29,14 +32,25 @@ export class SigninComponent implements OnInit {
     // console.log('signin', this.signinForm.value);
     // console.log('username', this.signinForm.value.email);
     // console.log('password', this.signinForm.value.password)
-    try {
-      // debugger;
-      this.authService.signIn(this.signinForm.value.email, this.signinForm.value.password);
-      this.router.navigateByUrl('home');
-    } catch(err) {
-      console.log('error', err);
-    }
+    // try {
+      debugger;
+      this.loading = true;
+      return this.authService.signIn(this.signinForm.value.email, this.signinForm.value.password)
+      .then( res => {
+        this.loading = false;
+        this.router.navigateByUrl('home');
+      })
+      .catch(
+        error => {this.errMsg = 'Incorrect username or passwrod. Please try again.';
+                this.loading = false; 
+              }//error.message
+      );
+      // this.router.navigateByUrl('home');
+    // } catch(err) {
+    //   console.log('error', err);
+    // }
     
-  }
+  // }
 
+  }
 }
