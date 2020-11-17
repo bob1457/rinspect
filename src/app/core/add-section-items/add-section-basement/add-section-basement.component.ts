1763b4dc8cebabe6fabda19c875f83f1de709609
bob <bob.yuan@yahoo.com>
2020-11-16 16:12:23 -0800

    add more sections.

--- src/app/core/add-section-items/add-section-basement/add-section-basement.component.ts
+++ src/app/core/add-section-items/add-section-basement/add-section-basement.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-add-section-basement',
@@ -7,9 +10,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSectionBasementComponent implements OnInit {
 
-  constructor() { }
+  @Input() rptId;
+
+  addMore = false;
+
+  basementForm: FormGroup;
+
+  codes = [
+    { 'name': 'G'},
+    { 'name': 'F'},
+    { 'name': 'P'},
+    { 'name': 'M'},
+    { 'name': 'D'},
+    { 'name': 'S'},
+    { 'name': 'B'},
+    { 'name': 'DT'},
+    { 'name': 'ST'}
+  ]
+
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private dataServie: DataService) { }
 
   ngOnInit(): void {
+    
+    this.basementForm = this.formBuilder.group({
+      name: [''],
+      type: [''],
+      // IN
+      conditionIn: this.formBuilder.group({
+        stairWayCode: [''],
+        stairWayCmnts: [''],
+        wallAndCarpetCode: [''],
+        wallAndCarpetCmnts: [''],
+        furnaceWaterPlumbingCode: [''],
+        furnaceWaterPlumbingCmnts: [''],
+        electricCmnts: [''],
+        electricCode: [''],        
+        lightingCmnts: [''],
+        lightingCode: [''],        
+        windowsCmnts: [''],
+        windowsCode: ['']
+      }),
+      //- OUT 
+
+      conditionOut: this.formBuilder.group({
+        stairWayCode: [''],
+        stairWayCmnts: [''],
+        wallAndCarpetCode: [''],
+        wallAndCarpetCmnts: [''],
+        furnaceWaterPlumbingCode: [''],
+        furnaceWaterPlumbingCmnts: [''],
+        electricCmnts: [''],
+        electricCode: [''],        
+        lightingCmnts: [''],
+        lightingCode: [''],        
+        windowsCmnts: [''],
+        windowsCode: ['']
+      })
+      
+      
+
+    })
   }
+
+  submit() {
+    this.basementForm.get('type').setValue('Basement');
+    console.log('add secton form', this.basementForm.value);
+    // call service to add section
+    console.log(this.rptId);
+
+    this.dataServie.createSection(this.basementForm.value, this.rptId);    
+
+    if (this.addMore) {
+      this.reloadComponent();
+    }else {
+      this.router.navigate(['home/report-details', this.rptId]);
+    }
+  }
+
+  reloadComponent() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/home/addSection', this.rptId]);
+  }
+
+  // clicked(event) {
+  //   this.addMore = event.checked;
+  //   console.log(this.addMore);
+  // }
 
 }
--- src/app/core/add-section-items/add-section-garbage-parking/add-section-garbage-parking.component.ts
+++ src/app/core/add-section-items/add-section-garbage-parking/add-section-garbage-parking.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-add-section-garbage-parking',
@@ -7,9 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSectionGarbageParkingComponent implements OnInit {
 
-  constructor() { }
+  @Input() rptId;
+
+  addMore = false;
+
+  garbageParkingForm: FormGroup;
+
+  codes = [
+    { 'name': 'G'},
+    { 'name': 'F'},
+    { 'name': 'P'},
+    { 'name': 'M'},
+    { 'name': 'D'},
+    { 'name': 'S'},
+    { 'name': 'B'},
+    { 'name': 'DT'},
+    { 'name': 'ST'}
+  ]
+
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private dataServie: DataService) { }
 
   ngOnInit(): void {
+    
+    this.garbageParkingForm = this.formBuilder.group({
+      name: [''],
+      type: [''],
+      isMaster: [false],
+      // IN
+      conditionIn: this.formBuilder.group({        
+        electricCmnts: [''],
+        electricCode: ['']
+      }),
+      //- OUT 
+
+      conditionOut: this.formBuilder.group({        
+        electricCmnts: [''],
+        electricCode: ['']
+      })
+      
+      
+
+    })
+  }
+
+  submit() {
+    this.garbageParkingForm.get('type').setValue('Garbage-Parking');
+    console.log('add secton form', this.garbageParkingForm.value);
+    // call service to add section
+    console.log(this.rptId);
+
+    this.dataServie.createSection(this.garbageParkingForm.value, this.rptId);    
+
+    if (this.addMore) {
+      this.reloadComponent();
+    }else {
+      this.router.navigate(['home/report-details', this.rptId]);
+    }
+  }
+
+  reloadComponent() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/home/addSection', this.rptId]);
   }
 
 }
