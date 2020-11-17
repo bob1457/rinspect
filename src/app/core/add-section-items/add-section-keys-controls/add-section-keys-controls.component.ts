79935115ea57944130e8de0b809161d1a93ba997
bob <bob.yuan@yahoo.com>
2020-11-17 11:06:09 -0800

    Finishing adding sections

--- src/app/core/add-section-items/add-section-keys-controls/add-section-keys-controls.component.ts
+++ src/app/core/add-section-items/add-section-keys-controls/add-section-keys-controls.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-add-section-keys-controls',
@@ -7,9 +10,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSectionKeysControlsComponent implements OnInit {
 
-  constructor() { }
+  @Input() rptId;
+
+  addMore = false;
+
+  keyControlForm: FormGroup;
+
+  keys = [
+    { 'number': 0},
+    { 'number': 1},
+    { 'number': 2},
+    { 'number': 3},
+    { 'number': 4},
+    { 'number': 5},
+    { 'number': 6},
+    { 'number': 7},
+    { 'number': 8},
+    { 'number': 9},
+    { 'number': 10}
+  ]
+
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private dataServie: DataService) { }
 
   ngOnInit(): void {
+    
+    this.keyControlForm = this.formBuilder.group({
+      name: [''],
+      type: [''],      
+      // IN
+      conditionIn: this.formBuilder.group({
+        buildingEntrandeKey: [0],
+        buildingEntrandeKeyCmnts: [''],
+        rentalUnitEntranceKey: [0],
+        rentalUnitEntranceCmnts: [''],
+        rentalUnityDeadBoltKey: [0],
+        rentalUnityDeadBoltCmnts: [''],
+        parkginRemoteControlKey: [0],
+        parkginRemoteControlCmnts: ['']
+      }),
+      //- OUT 
+
+      conditionOut: this.formBuilder.group({
+        buildingEntrandeKey: [0],
+        buildingEntrandeKeyCmnts: [''],
+        rentalUnitEntranceKey: [0],
+        rentalUnitEntranceCmnts: [''],
+        rentalUnityDeadBoltKey: [0],
+        rentalUnityDeadBoltCmnts: [''],
+        parkginRemoteControlKey: [0],
+        parkginRemoteControlCmnts: ['']
+      })
+      
+      
+
+    })
+  }
+
+  submit() {
+    this.keyControlForm.get('type').setValue('Key-Control');
+    console.log('add secton form', this.keyControlForm.value);
+    // call service to add section
+    console.log(this.rptId);
+
+    this.dataServie.createSection(this.keyControlForm.value, this.rptId);    
+
+    if (this.addMore) {
+      this.reloadComponent();
+    }else {
+      this.router.navigate(['home/report-details', this.rptId]);
+    }
+  }
+
+  reloadComponent() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/home/addSection', this.rptId]);
   }
 
 }
