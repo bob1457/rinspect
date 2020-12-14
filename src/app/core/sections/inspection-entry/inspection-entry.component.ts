d26ed398db72d6283e770052b0d3e6096ccd5588
bob <bob.yuan@yahoo.com>
2020-12-14 10:29:55 -0800

    Refacoring section ui

--- src/app/core/sections/inspection-entry/inspection-entry.component.ts
+++ src/app/core/sections/inspection-entry/inspection-entry.component.ts
@@ -45,43 +45,7 @@ export class InspectionEntryComponent implements OnInit {
 
   ngOnInit(): void {
     // console.log('section', this.entrySectionDetails);
-    this.entryForm = this.formBuilder.group({
-      // Temp
-      // code: ['G'],
-      // comments: ['N/A'],
-      // name: [''],
-      // type: [''],
-      // // IN
-      // cellingCmnts_in: [''],
-      // cellingCode_in: [''],
-      // closetsCode_in: [''],
-      // closetsCmnts_in: [''],
-      // electricCmnts_in: [''],
-      // electricCode_in: [''],
-      // floorCmnts_in: [''],
-      // floorCode_in: [''],
-      // lightingCmnts_in: [''],
-      // lightingCode_in: [''],
-      // wallTrimCmnts_in: [''],
-      // wallTrimCode_in: [''],
-      // windowsCmnts_in: [''],
-      // windowsCode_in: [''],
-
-      // //- OUT
-      // cellingCmnts_out: [''],
-      // cellingCode_out: [''],
-      // closetsCode_out: [''],
-      // closetsCmnts_out: [''],      
-      // electricCmnts_out: [''],
-      // electricCode_out: [''],
-      // floorCmnts_out: [''],
-      // floorCode_out: [''],
-      // lightingCmnts_out: [''],
-      // lightingCode_out: [''],
-      // wallTrimCmnts_out: [''],
-      // wallTrimCode_out: [''],
-      // windowsCmnts_out: [''],
-      // windowsCode_out: ['']
+    this.entryForm = this.formBuilder.group({     
 
       name: [''],
       type: [''],
@@ -123,49 +87,50 @@ export class InspectionEntryComponent implements OnInit {
 
     });
 
-    this.entrySectionDetails.forEach(element => {
-        this.entryForm.get('name').setValue(element.payload.doc.data().name);
-        // this.entryForm.get('type').disable(element.paayload.doc().data().type);
-      // Move In data
-      this.entryForm.get('conditionIn').get('cellingCmnts').setValue(element.payload.doc.data().conditionIn.cellingCmnts);
-      this.entryForm.get('conditionIn').get('closetsCmnts').setValue(element.payload.doc.data().conditionIn.closetsCmnts);
-      this.entryForm.get('conditionIn').get('closetsCode').setValue(element.payload.doc.data().conditionIn.closetsCode);
-      this.entryForm.get('conditionIn').get('cellingCode').setValue(element.payload.doc.data().conditionIn.cellingCode);
-      this.entryForm.get('conditionIn').get('electricCmnts').setValue(element.payload.doc.data().conditionIn.electricCmnts);
-      this.entryForm.get('conditionIn').get('electricCode').setValue(element.payload.doc.data().conditionIn.electricCode);
-      this.entryForm.get('conditionIn').get('floorCode').setValue(element.payload.doc.data().conditionIn.floorCode);
-      this.entryForm.get('conditionIn').get('floorCmnts').setValue(element.payload.doc.data().conditionIn.floorCmnts);
-      this.entryForm.get('conditionIn').get('lightingCmnts').setValue(element.payload.doc.data().conditionIn.lightingCmnts);
-      this.entryForm.get('conditionIn').get('lightingCode').setValue(element.payload.doc.data().conditionIn.lightingCode);
-      this.entryForm.get('conditionIn').get('windowsCmnts').setValue(element.payload.doc.data().conditionIn.windowsCmnts);
-      this.entryForm.get('conditionIn').get('windowsCode').setValue(element.payload.doc.data().conditionIn.windowsCode);
-      this.entryForm.get('conditionIn').get('wallTrimCode').setValue(element.payload.doc.data().conditionIn.wallTrimCode);
-      this.entryForm.get('conditionIn').get('wallTrimCmnts').setValue(element.payload.doc.data().conditionIn.wallTrimCmnts);
+    // debugger;
+    // this.entrySectionDetails.forEach(element => {
+    //     this.entryForm.get('name').setValue(element.payload.doc.data().name);
+    //     // this.entryForm.get('type').disable(element.paayload.doc().data().type);
+    //   // Move In data
+    //   this.entryForm.get('conditionIn').get('cellingCmnts').setValue(element.payload.doc.data().conditionIn.cellingCmnts);
+    //   this.entryForm.get('conditionIn').get('closetsCmnts').setValue(element.payload.doc.data().conditionIn.closetsCmnts);
+    //   this.entryForm.get('conditionIn').get('closetsCode').setValue(element.payload.doc.data().conditionIn.closetsCode);
+    //   this.entryForm.get('conditionIn').get('cellingCode').setValue(element.payload.doc.data().conditionIn.cellingCode);
+    //   this.entryForm.get('conditionIn').get('electricCmnts').setValue(element.payload.doc.data().conditionIn.electricCmnts);
+    //   this.entryForm.get('conditionIn').get('electricCode').setValue(element.payload.doc.data().conditionIn.electricCode);
+    //   this.entryForm.get('conditionIn').get('floorCode').setValue(element.payload.doc.data().conditionIn.floorCode);
+    //   this.entryForm.get('conditionIn').get('floorCmnts').setValue(element.payload.doc.data().conditionIn.floorCmnts);
+    //   this.entryForm.get('conditionIn').get('lightingCmnts').setValue(element.payload.doc.data().conditionIn.lightingCmnts);
+    //   this.entryForm.get('conditionIn').get('lightingCode').setValue(element.payload.doc.data().conditionIn.lightingCode);
+    //   this.entryForm.get('conditionIn').get('windowsCmnts').setValue(element.payload.doc.data().conditionIn.windowsCmnts);
+    //   this.entryForm.get('conditionIn').get('windowsCode').setValue(element.payload.doc.data().conditionIn.windowsCode);
+    //   this.entryForm.get('conditionIn').get('wallTrimCode').setValue(element.payload.doc.data().conditionIn.wallTrimCode);
+    //   this.entryForm.get('conditionIn').get('wallTrimCmnts').setValue(element.payload.doc.data().conditionIn.wallTrimCmnts);
 
-      // Move Out data
-      this.entryForm.get('conditionOut').get('cellingCmnts').setValue(element.payload.doc.data().conditionOut.cellingCmnts);
-      this.entryForm.get('conditionOut').get('closetsCmnts').setValue(element.payload.doc.data().conditionOut.closetsCmnts);
-      this.entryForm.get('conditionOut').get('closetsCode').setValue(element.payload.doc.data().conditionOut.closetsCode);
-      this.entryForm.get('conditionOut').get('cellingCode').setValue(element.payload.doc.data().conditionOut.cellingCode);
+    //   // Move Out data
+    //   this.entryForm.get('conditionOut').get('cellingCmnts').setValue(element.payload.doc.data().conditionOut.cellingCmnts);
+    //   this.entryForm.get('conditionOut').get('closetsCmnts').setValue(element.payload.doc.data().conditionOut.closetsCmnts);
+    //   this.entryForm.get('conditionOut').get('closetsCode').setValue(element.payload.doc.data().conditionOut.closetsCode);
+    //   this.entryForm.get('conditionOut').get('cellingCode').setValue(element.payload.doc.data().conditionOut.cellingCode);
 
-      this.entryForm.get('conditionOut').get('electricCmnts').setValue(element.payload.doc.data().conditionOut.electricCmnts);
-      this.entryForm.get('conditionOut').get('electricCode').setValue(element.payload.doc.data().conditionOut.electricCode);
-      this.entryForm.get('conditionOut').get('floorCode').setValue(element.payload.doc.data().conditionOut.floorCode);
-      this.entryForm.get('conditionOut').get('floorCmnts').setValue(element.payload.doc.data().conditionOut.floorCmnts);
-      this.entryForm.get('conditionOut').get('lightingCmnts').setValue(element.payload.doc.data().conditionOut.lightingCmnts);
-      this.entryForm.get('conditionOut').get('lightingCode').setValue(element.payload.doc.data().conditionOut.lightingCode);
-      this.entryForm.get('conditionOut').get('windowsCmnts').setValue(element.payload.doc.data().conditionOut.windowsCmnts);
-      this.entryForm.get('conditionOut').get('windowsCode').setValue(element.payload.doc.data().conditionOut.windowsCode);
-      this.entryForm.get('conditionOut').get('wallTrimCode').setValue(element.payload.doc.data().conditionOut.wallTrimCode);
-      this.entryForm.get('conditionOut').get('wallTrimCmnts').setValue(element.payload.doc.data().conditionOut.wallTrimCmnts);
+    //   this.entryForm.get('conditionOut').get('electricCmnts').setValue(element.payload.doc.data().conditionOut.electricCmnts);
+    //   this.entryForm.get('conditionOut').get('electricCode').setValue(element.payload.doc.data().conditionOut.electricCode);
+    //   this.entryForm.get('conditionOut').get('floorCode').setValue(element.payload.doc.data().conditionOut.floorCode);
+    //   this.entryForm.get('conditionOut').get('floorCmnts').setValue(element.payload.doc.data().conditionOut.floorCmnts);
+    //   this.entryForm.get('conditionOut').get('lightingCmnts').setValue(element.payload.doc.data().conditionOut.lightingCmnts);
+    //   this.entryForm.get('conditionOut').get('lightingCode').setValue(element.payload.doc.data().conditionOut.lightingCode);
+    //   this.entryForm.get('conditionOut').get('windowsCmnts').setValue(element.payload.doc.data().conditionOut.windowsCmnts);
+    //   this.entryForm.get('conditionOut').get('windowsCode').setValue(element.payload.doc.data().conditionOut.windowsCode);
+    //   this.entryForm.get('conditionOut').get('wallTrimCode').setValue(element.payload.doc.data().conditionOut.wallTrimCode);
+    //   this.entryForm.get('conditionOut').get('wallTrimCmnts').setValue(element.payload.doc.data().conditionOut.wallTrimCmnts);
 
-      this.sectionId = element.payload.doc.id;
-      console.log('sec id', element.payload.doc.id);
-      console.log('sec id to password', element.payload.doc.id);
-      console.log('report id', this.reportId);
-    });
+    //   this.sectionId = element.payload.doc.id;
+    //   console.log('sec id', element.payload.doc.id);
+    //   // console.log('sec id to password', element.payload.doc.id);
+    //   console.log('report id', this.reportId);
+    // });
 
-    
+    console.log('entry-details', this.entrySectionDetails);
   }
 
   // Update section
