2bb866535b8642395885cc7e866508637b918bd8
Bob Yuan <yuanbob@hotmail.com>
2021-02-19 13:35:28 -0800

    Add more features on report view items

--- src/app/core/report-view-header/report-view-header.component.ts
+++ src/app/core/report-view-header/report-view-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-report-view-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-view-header.component.scss']
 })
 export class ReportViewHeaderComponent implements OnInit {
+
+  @Input() note = '';
 
   constructor() { }
 
